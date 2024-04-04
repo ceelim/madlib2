2257f502b64b9eb934d410bc47ace579a35cab03
ceelim <165067442+ceelim@users.noreply.github.com>
2024-04-04 22:00:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function MadLibTime() {
+  var adj1 = document.getElementById("adj1").value;
+  var adj2 = document.getElementById("adj2").value;
+  var noun1 = document.getElementById("noun1").value;
+  var verb1 = document.getElementById("verb1").value;
+  var adj3 = document.getElementById("adj3").value;
+  var noun2 = document.getElementById("noun2").value;
+  var adj4 = document.getElementById("adj4").value;
+  var noun3 = document.getElementById("noun3").value;
+  var verb2 = document.getElementById("verb2").value;
+  var noun4 = document.getElementById("noun4").value;
+  var verb3 = document.getElementById("verb3").value;
+  var noun5 = document.getElementById("noun5").value;
+  var verb4 = document.getElementById("verb4").value;
+  var adj5 = document.getElementById("adj5").value;
+  var adj6 = document.getElementById("adj6").value;
+
+  document.write( "Ready for the whirlwind of newborn care as  a new parent? Hope these 10 tips can help... 1. Change diapers frequently to keep baby " + adj1 + " and dry." + "  2. Use " + adj2 + " products for their sensitive " + noun1 + ".  3."+ verb1 + " a " + adj3 + " and safe sleeping " + noun2 + " for baby.  4. Find a " + adj4 + " spot in your " + noun3 + " to bond while feeding your child.  5. Try to " + verb2 + " when the baby sleeps.  6. Accept help with household " + noun4 + " and errands.  7. Just " + verb3 + " your instincts.  8. Make " + noun5 + " for self-care.  9. Stay hydrated and make time to " + verb4 + " daily.  10. Cherish all the " + adj5 + " moments with your " + adj6 + " one! Welcome to Parenthood!"); 
+}
